0c39592c37a1507a50cfde1eb1908abc60540684
tan <me@mlourdestan.com>
2024-07-20 23:11:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
         name: "single origin",
         rank: 1
         favorite drink: flat white
+             };
+     const coffeeShop = {
         name: "deuces",
         rank: 2
         favorite drink: seasalt caramel latte
-        name: "sspace",
-        rank: 3
-        favorite drink: flat white
+             };
+     const coffeeShop = {
         name: "candid coffee",
-        rank: 4
+        rank: 3
         favorite drink: vietnamese latte
+             };
+     const coffeeShop = {
+        name: "sspace coffee + bar",
+        rank: 4
+        favorite drink: spanish latte
+             };
+     const coffeeShop = {
         name: "salakot",
         rank: 5
         favorite drink: spanish latte
-    };
-
+             };
     rankingDiv.innerHTML = `
         <p><strong>${coffeeShop.name}</strong> is ranked #${coffeeShop.rank} in ${coffeeShop.year}!</p>
     `;
